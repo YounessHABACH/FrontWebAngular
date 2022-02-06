c9326fbe368a6c4c7315fc37bd321c8d968f3ce2
Youness HABACH <youness.habach@gmail.com>
2022-02-06 18:31:22 +0100

    Angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule, Routes} from "@angular/router";
-// laibriries
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {FlexLayoutModule} from "@angular/flex-layout";
+import { RouterModule, Routes} from "@angular/router";
+// laibrairies
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatListModule } from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import 'hammerjs'
 // components
 import { AppComponent } from './app.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { MenuComponent } from './menu/menu.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 const routes: Routes = [
 
@@ -14,15 +21,21 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishdetailComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
+  //RouterModule.forRoot(routes),
   providers: [],
   bootstrap: [AppComponent]
 })
